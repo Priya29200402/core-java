922cb0e67d2d08bde5b2c0ca46738d039a27814d
Priya29200402 <mudalagipriya@gmail.com>
2026-04-23 22:00:49 +0530

    String read text file program

---
+++ string/ReadFile.java
@@ -0,0 +1,24 @@
+package com.xworkz.practice;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        try {
+            FileReader fr = new FileReader("output.txt");
+            BufferedReader br = new BufferedReader(fr);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+            e.printStackTrace();
+        }
+    }
+}
