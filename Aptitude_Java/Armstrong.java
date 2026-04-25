7b0e414e9a970093d81f0c3ceeedf6fe2363376c
Priya29200402 <mudalagipriya@gmail.com>
2026-04-25 16:12:29 +0530

    Java aptitude programs

---
+++ Aptitude_Java/Armstrong.java
@@ -0,0 +1,16 @@
+class Armstrong {
+    public static void main(String[] args) {
+        int num = 153, sum = 0, original = num;
+
+        while(num != 0) {
+            int digit = num % 10;
+            sum += digit * digit * digit;
+            num /= 10;
+        }
+
+        if(sum == original)
+            System.out.println("Armstrong");
+        else
+            System.out.println("Not Armstrong");
+    }
+}
