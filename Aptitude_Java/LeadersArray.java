b1e5ae61984dd8b99e8b82f30834465345162a7e
Priya29200402 <mudalagipriya@gmail.com>
2026-05-04 12:09:41 +0530

    Java aptitude programs

---
+++ Aptitude_Java/LeadersArray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class LeadersArray{
+    public static void main(String[] args) {
+        int[] arr = {16, 17, 4, 3, 5, 2};
+
+        List<Integer> leaders = new ArrayList<>();
+        int maxFromRight = arr[arr.length - 1];
+
+        leaders.add(maxFromRight);
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] > maxFromRight) {
+                maxFromRight = arr[i];
+                leaders.add(arr[i]);
+            }
+        }
+
+        Collections.reverse(leaders);
+        System.out.println(leaders);
+    }
+}
