f5705e77be7780e1ba9878d09d84ad994a9540ad
Priya29200402 <mudalagipriya@gmail.com>
2026-05-28 09:23:49 +0530

    Nandini milk project

---
+++ nandini/service/DamagedProductDetailService.java
@@ -0,0 +1,8 @@
+package com.xworkz.nandini.service;
+
+import com.xworkz.nandini.dto.DamagedProductDetailDTO;
+
+public interface DamagedProductDetailService {
+    void validateAndSave(DamagedProductDetailDTO detailDTO);
+
+}
