0547d55c23c2b87fb0bb03b6e3007dab2337e02e
Priya29200402 <mudalagipriya@gmail.com>
2026-06-01 10:51:45 +0530

    Nandini milk project

--- nandini/service/OrderDetailService.java
+++ nandini/service/OrderDetailService.java
@@ -3,5 +3,5 @@ package com.xworkz.nandini.service;
 import com.xworkz.nandini.dto.OrderDetailDTO;
 
 public interface OrderDetailService {
-    void validateAndSave(OrderDetailDTO orderDetailDTO);
+    boolean validateAndSave(OrderDetailDTO orderDetailDTO);
 }
--- nandini/service/OrderDetailServiceImp.java
+++ nandini/service/OrderDetailServiceImp.java
@@ -4,7 +4,25 @@ import com.xworkz.nandini.dto.OrderDetailDTO;
 
 public class OrderDetailServiceImp implements OrderDetailService {
     @Override
-    public void validateAndSave(OrderDetailDTO orderDetailDTO) {
+    public boolean validateAndSave(OrderDetailDTO orderDetailDTO) {
         System.out.println("Running validate and Save in OrderDetailDTO: " + orderDetailDTO);
+        String orderName = orderDetailDTO.getOrderName();
+        if (orderName != null && orderName.length() >= 3 && orderName.length() <= 100 && orderName.matches("[a-zA-Z ,]+")) {
+            System.out.println("valid orderName");
+        } else {
+            System.out.println("invalid orderName");
+            return false;
+        }
+
+        if (orderDetailDTO.getOrderPrice()>5)
+        {
+            System.out.println("valid orderPrice");
+            return false;
+        }
+        else {
+            System.out.println("Invalid orderPrice");
+        }
+        System.out.println("validation is completed");
+        return false;
     }
 }
